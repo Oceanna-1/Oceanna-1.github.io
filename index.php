357a35f21952f90e03e8417d48fa16be4cab2992
Oceanna-1 <oceannahartog@gmail.com>
2025-02-03 22:27:59 -0500

    implemented php for footer and header  on main index

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!-- Author: Oceanna Hartog; 000940535-->
+ <!-- Landing Page containing previously submitted assignments for my client side course-->
+
+ <?php include("./nav.html")?>
+    
+    <div class="row">
+        <div class="col">
+            <img src="images/pumpkincat.jpg">
+        </div>
+        <div class="col">
+                <img src="images/junimo.gif">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <img src="images/ghost.gif">
+        </div>
+        <div class="col" id="col2">
+            <div class="pet">
+                <img src="images/rowan.jpg" alt="orange cat">
+                <p>Rowan</p>
+            </div>
+            <div class="pet">
+                <img src="images/fawkes.jpg" alt="orange cat">
+                <p>Fawkes</p>
+            </div>
+            <div class="pet">
+                <img src = "images/ellie.jpg" alt="orange and white dog sleeping">
+                <p>Ellie</p>
+            </div>
+        </div>
+    </div>
+    <?php include("./footer.html")?>
